a051a4d63830ba5c7274b85c3ec6e49dbb9acf40
Jennifer Gomez <jennifergomezbruno@gmail.com>
2019-11-02 17:15:52 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -11,7 +11,7 @@ end
 def print_first_directors_movie_titles
   row_index = 0 
   while row_index < directors_database.length do 
-    puts directors_database[row_index][:title]
+    puts directors_database[row_index][:titles]
     row_index += 1 
   end  
   
